64d2e1ecf55960f9f216bd07e73b1cf919dfa4a6
Smith, Cooper <cooper.smith@jwt.com>
2015-04-01 18:19:41 -0400

    added superselector to expert section

--- app.js
+++ app.js
@@ -60,69 +60,59 @@ var ctgArr =[{"cat": "Science"},
 
 //Our expert controller
 testApp.controller('expertCtrl', function($scope, $http) {
-console.log ("My expert Controller is in control!")
-
 var expertData = new Array();
 var optionNo = 0;
 var firsttime = true;
 
-var ctgArr =[{"id":"1", "cat": "Science"},
-				{"id":"2","cat":"Design"},
-				{"id":"3","cat":"Engineering"},
-				{"id":"4","cat": "Finance"},
-				{"id":"5","cat":"Food"}];
+var ctgArr =[ {"id":"0", "cat": "Please select category, untill all categories are selected..."},
+        {"id":"1", "cat": "Science"},
+        {"id":"2","cat":"Design"},
+        {"id":"3","cat":"Engineering"},
+        {"id":"4","cat": "Finance"},
+        {"id":"5","cat":"Food"}];
 
  console.log("Set up categories: ", ctgArr);
 
    var options = ctgArr;
     // make sure the selector get the final options
-        $scope.options = options;
-        $scope.selectedOption = $scope.options[0]; 
+       $scope.options = options;
+       $scope.selectedOption = $scope.options[0];
 
-if ( !firsttime) {
  $scope.$watch('selectedOption', function(newValue, oldValue) {
                     // handle selection change ...
-                    console.log("expertController - selection: " , $scope.selectedOption);
+                    var selectObj;
+                    selectObj = $scope.selectedOption;
+
                     var newOptions = new Array(); 
                     var option = $scope.selectedOption;
-
-                    console.log("choice number: " + optionNo+ " option selected: ", option );
-                    console.log("current options: " + $scope.options[optionNo].cat );
-
-                    expertData[optionNo] = option;
-                    $scope.selectedOption = $scope.options[0];
-
-                //reset option array to exclude selection / add selection to profile array FIFO 
-                     for ( var i=0 ; i <  $scope.options.length ; i++) {
-                        if ( i !=  optionNo ) {
-                            console.log("ExpertController - sending country: " + $scope.options[i].cat );
-                            newOptions.push($scope.options[i]);
-                            //$scope.country  = countryIds[i].country;
-                            //$rootScope.country = $scope.country;
-                        } //end if          
-                    }//end for loop..*/
+                  
                     ++optionNo;
-                    console.log("new options list: " , newOptions );
-                    $scope.options = newOptions;
-                    //$scope.options = options;
-                    $scope.selectedOption = $scope.options[i];
-                });//end watch selectedOption 
-}//end if on watch
 
-firsttime = false;
+                    if ( (optionNo != 0) && ($scope.selectedOption.id != 0 )) {
+                      for ( var i=0 ; i <  $scope.options.length ; i++) {
+                        if ( $scope.options[i].cat != $scope.selectedOption.cat ) {
+                           newOptions.push($scope.options[i]);
+                        } else {
+                            expertData.push({id: optionNo, cat: $scope.options[i].cat });
+                        }//end if          
+                      } //end for
+  
+                      $scope.options = newOptions;
+                      $scope.selectedOption = newOptions[0];
+                    }//end iff 
+                   
+                });//end watch selectedOption 
+
 
  $scope.showProfile = function () {
-   	 console.log("getProfile - Build our user profile: " , expertData[1].cat );
 
-   	 var showArr = new Array();
+     var showArr = new Array();
+     for ( var i=0; i < expertData.length ; ++i ) {
+        showArr[i] = expertData[i].cat;
+     }
 
-   	 for (var i=1 ;i < expertData.length ; i++) {
-   	 	console.log("preference: " , expertData[i] );
-   	 	showArr[i] = expertData[i].cat+" ";
-   	 }
-
-   	 alert("Here is your profile preferences: " + showArr );
-   	 alert("Get ready for those emails!!!");
+     alert("Here is your profile preferences: " + showArr );
+     alert("Get ready for those emails!!!"); 
   }//end getData
 
 });//end controller
